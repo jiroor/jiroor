a182fe5f83c2fc55f69a6ba7077c92e5dc3d6b9d
jiroor <thx.1by1.or25@gmail.com>
2019-08-04 19:47:40 +0900

    feat(firebase): add firebase

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,6 +1,19 @@
 import Vue, { ComponentOptions } from 'vue';
 import VueRouter from 'vue-router';
 
+// firebase
+import * as firebase from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCo29810ZYi_lPvsVC5bwb2Q6zno-wajoY',
+  authDomain: 'jiroor-a0000.firebaseapp.com',
+  databaseURL: 'https://jiroor-a0000.firebaseio.com',
+  projectId: 'jiroor-a0000',
+  storageBucket: '',
+  messagingSenderId: '575059976341',
+  appId: '1:575059976341:web:7845135fa1a90d97'
+};
+firebase.initializeApp(firebaseConfig);
+
 // page/top
 const Top: ComponentOptions<Vue> = {
   template: '<div>top</div>',
