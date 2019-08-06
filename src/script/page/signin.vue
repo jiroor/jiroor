71b607d2bea0ac235e6550f4fa4beb09dd65e14c
jiroor <thx.1by1.or25@gmail.com>
2019-08-06 18:21:23 +0900

    feat(signin): add feature to signin

--- src/script/page/signin.vue
+++ src/script/page/signin.vue
@@ -5,20 +5,41 @@
     <form>
       <input type="text" placeholder="Username" v-model="username" />
       <input type="password" placeholder="Password" v-model="password" />
-      <button>Signin</button>
+      <button @click="signin">Signin</button>
+      <p v-show="error">{{ error }}</p>
     </form>
   </section>
 </template>
 
 <script lang="ts">
 import Vue, { ComponentOptions } from "vue";
+import * as firebase from "firebase";
 
-const Signin: ComponentOptions<Vue> = {
+interface ISignin extends Vue {
+  username: string;
+  password: string;
+  error: string;
+}
+
+const Signin: ComponentOptions<ISignin> = {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      error: ""
     };
+  },
+
+  methods: {
+    signin() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.username, this.password)
+        .then((user: firebase.auth.UserCredential) => {
+          this.$router.push("top");
+        })
+        .catch((error: Error) => (this.error = error.message));
+    }
   }
 };
 
